28457d0654e74b065cbcf5975066e3a623867837
peter08-araujo <spthurzin777@gmail.com>
2025-03-27 14:01:50 -0300

    Create codel.js

---
+++ codel.js
@@ -0,0 +1,9 @@
+!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2T9H0BD35M"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2T9H0BD35M');
+</script>
